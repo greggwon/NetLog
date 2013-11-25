c87f4c0c4427c8d004232195794f5edd1c274143
Gregg Wonderly <gregg@wonderly.org>
2013-11-24 20:55:35 -0600

    fix for x64 from internet
    http://code.google.com/p/zeroconfignetservices/issues/detail?id=4

--- zeroconfservice/ZeroconfService/UnixSocket.cs
+++ zeroconfservice/ZeroconfService/UnixSocket.cs
@@ -5,47 +5,39 @@ using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Threading;
 
-namespace ZeroconfService
-{
-	public class UnixSocket
-	{
-		private Int32 mSocket;
+namespace ZeroconfService {
+	public class UnixSocket {
+		private UIntPtr mSocket;
 
 		// Delegate to allow asynchronous calling of the poll method
-		private delegate bool AsyncPollCaller(int microSeconds, SelectMode mode);
+		private delegate bool AsyncPollCaller( int microSeconds, SelectMode mode );
 		private AsyncPollCaller caller;
 
-		public UnixSocket(Int32 socket)
-		{
+		public UnixSocket( UIntPtr socket ) {
 			mSocket = socket;
 
 			caller = new AsyncPollCaller(Poll);
 		}
 
-		public virtual IAsyncResult BeginPoll(int microSeconds, SelectMode mode, AsyncCallback callback, Object state)
-		{
+		public virtual IAsyncResult BeginPoll( int microSeconds, SelectMode mode, AsyncCallback callback, Object state ) {
 			IAsyncResult result = caller.BeginInvoke(microSeconds, mode, callback, state);
 			return result;
 		}
 
-		public virtual bool EndPoll(IAsyncResult asyncResult)
-		{
+		public virtual bool EndPoll( IAsyncResult asyncResult ) {
 			return caller.EndInvoke(asyncResult);
 		}
 
-		protected bool Poll(int microSeconds, SelectMode mode)
-		{
+		protected bool Poll( int microSeconds, SelectMode mode ) {
 			fd_set readFDs = null;
 			fd_set writeFDs = null;
 
-			if (mode == SelectMode.SelectRead)
-			{
+			if( mode == SelectMode.SelectRead ) {
 				readFDs = new fd_set();
 				readFDs.FD_ZERO();
 				readFDs.FD_SET(mSocket);
 			}
-			if (mode == SelectMode.SelectWrite)
-			{
+			if( mode == SelectMode.SelectWrite ) {
 				writeFDs = new fd_set();
 				writeFDs.FD_ZERO();
 				writeFDs.FD_SET(mSocket);
@@ -55,8 +47,7 @@ namespace ZeroconfService
 
 			//Console.WriteLine("select returned: {0}", ret);
 
-			if (readFDs.FD_ISSET(mSocket))
-			{
+			if( readFDs.FD_ISSET(mSocket) ) {
 				return true;
 			}
 			return false;
@@ -64,44 +55,34 @@ namespace ZeroconfService
 
 		/* unmanaged stuff */
 		[StructLayout(LayoutKind.Sequential)]
-		private unsafe class fd_set
-		{
+		private unsafe class fd_set {
 			public UInt32 fd_count;
 			[MarshalAs(UnmanagedType.ByValArray, SizeConst = 64)]
-			public Int32[] fd_array;
+			public UIntPtr[] fd_array;
 
-			public fd_set()
-			{
-				fd_array = new Int32[64];
+			public fd_set() {
+				fd_array = new UIntPtr[ 64 ];
 			}
 
-			public void FD_ZERO()
-			{
+			public void FD_ZERO() {
 				fd_count = 0;
 			}
-			public void FD_SET(Int32 fd)
-			{
+			public void FD_SET( UIntPtr fd ) {
 				int i;
-				for (i = 0; i < fd_count; i++)
-				{
-					if (fd_array[i] == fd) break;
+				for( i = 0; i < fd_count; i++ ) {
+					if( fd_array[ i ] == fd ) break;
 				}
-				if (i == fd_count)
-				{
-					fd_array[i] = fd;
+				if( i == fd_count ) {
+					fd_array[ i ] = fd;
 					fd_count++;
 				}
 			}
-			public void FD_CLR(Int32 fd)
-			{
+			public void FD_CLR( UIntPtr fd ) {
 				int i;
-				for (i = 0; i < fd_count; i++)
-				{
-					if (fd_array[i] == fd)
-					{
-						while (i < (fd_count - 1))
-						{
-							fd_array[i] = fd_array[i + 1];
+				for( i = 0; i < fd_count; i++ ) {
+					if( fd_array[ i ] == fd ) {
+						while( i < ( fd_count - 1 ) ) {
+							fd_array[ i ] = fd_array[ i + 1 ];
 							i++;
 						}
 						fd_count--;
@@ -109,70 +90,61 @@ namespace ZeroconfService
 					}
 				}
 			}
-			public bool FD_ISSET(Int32 fd)
-			{
-				return (__WSAFDIsSet(fd, this) != 0);
+			public bool FD_ISSET( UIntPtr fd ) {
+				return ( __WSAFDIsSet(fd, this) != 0 );
 			}
 		}
 
-		private unsafe class timeval
-		{
+		private unsafe class timeval {
 		}
 
 		[DllImport("Ws2_32.dll")]
-		private static extern Int32 __WSAFDIsSet(Int32 fd, fd_set set);
+		private static extern Int32 __WSAFDIsSet( UIntPtr fd, fd_set set );
 
 		[DllImport("Ws2_32.dll")]
-		private static extern Int32 select(Int32 nfds, fd_set readFDs, fd_set writeFDs, fd_set exceptFDs, timeval timeout);
+		private static extern Int32 select( Int32 nfds, fd_set readFDs, fd_set writeFDs, fd_set exceptFDs, timeval timeout );
 
 		[DllImport("Ws2_32.dll")]
 		private static extern Int32 WSAGetLastError();
 	}
 
-	class WatchSocket : UnixSocket
-	{
+	class WatchSocket: UnixSocket {
 		private IntPtr sdRef;
 		private bool inPoll;
 		private bool stopping;
 
-		public WatchSocket(Int32 socket, IntPtr sdRef)
-			: base(socket)
-		{
+		public WatchSocket( UIntPtr socket, IntPtr sdRef )
+			: base(socket) {
 			this.sdRef = sdRef;
 			this.inPoll = false;
 			this.stopping = false;
 		}
 
-		public IntPtr SDRef
-		{
+		public IntPtr SDRef {
 			get { return sdRef; }
 		}
 
-		public bool InPoll
-		{
+		public bool InPoll {
 			get { return inPoll; }
 		}
 
-		public override IAsyncResult BeginPoll(int microSeconds, SelectMode mode, AsyncCallback callback, Object state)
-		{
-			if(inPoll)
+		public override IAsyncResult BeginPoll( int microSeconds, SelectMode mode, AsyncCallback callback, Object state ) {
+			if( inPoll )
 				throw new ApplicationException("Attempting to begin a new poll while already polling.");
 
-			if(stopping)
-                throw new ApplicationException("Attempting to begin a new poll after stopping.");
+			if( stopping )
+				throw new ApplicationException("Attempting to begin a new poll after stopping.");
 
 			inPoll = true;
 			return base.BeginPoll(microSeconds, mode, callback, state);
 		}
 
-		public override bool EndPoll(IAsyncResult asyncResult)
-		{
+		public override bool EndPoll( IAsyncResult asyncResult ) {
 			inPoll = false;
 			return base.EndPoll(asyncResult);
 		}
 
-		public bool Stopping
-		{
+		public bool Stopping {
 			get { return stopping; }
 			set { stopping = value; }
 		}
