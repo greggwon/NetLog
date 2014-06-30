c95a2f73c045a5be18b00cf20093fd74de65766a
Gregg Wonderly <gregg@wonderly.org>
2014-06-30 17:39:33 -0500

    add

---
+++ Testing/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.IO;
+using System.Diagnostics;
+using System.Globalization;
+using NetLog.Logging;
+
+namespace NetLog.Logging.Testing {
+	class Program  {
+		public static void Main(string[] args) {
+			Logger log = Logger.GetLogger("iWellScada.logging.kern");
+			log.info("kern logging"); 
+		}
+	}
+}
